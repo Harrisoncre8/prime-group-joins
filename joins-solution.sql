6d29acbb083bf9775ebbc05caf960ba432a519e1
Harrison Nguyen <nguy2173@umn.edu>
2019-12-17 16:36:26 -0600

    SQL query check warehouse products

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,16 +1,29 @@
 -- BASE MODE
 
 -- 1. Get all customers and their addresses.
-SELECT * FROM "addresses"
+SELECT "addresses"."street", "addresses"."city", "addresses"."state", "addresses"."zip", "addresses"."address_type", "customers"."first_name", "customers"."last_name"
+FROM "addresses"
 JOIN "customers" ON "customers"."id" = "addresses"."customer_id" 
-GROUP BY "addresses"."id", "customers"."id"
 ORDER BY "addresses"."id";
 
 -- 2. Get all orders and their line items (orders, quantity and product).
+SELECT * FROM "line_items"
+JOIN "orders" ON "orders"."id" = "line_items"."order_id"
+JOIN "products" ON "products"."id" = "line_items"."product_id";
 
 -- 3. Which warehouses have cheetos?
+SELECT * FROM "warehouse_product"
+JOIN "warehouse" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'cheetos';
+-- delta
 
 -- 4. Which warehouses have diet pepsi?
+SELECT * FROM "warehouse_product"
+JOIN "warehouse" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
+-- alpha, delta, gamma
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
